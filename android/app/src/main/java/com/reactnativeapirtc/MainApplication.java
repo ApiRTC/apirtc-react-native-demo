e95aee8d7faba079b0d9cbaced3f5d697965f70e
minhduc <minhduc.hua@gmail.com>
2017-11-02 13:08:14 +0100

    chore(packages): install react-native-vector-icons

--- android/app/src/main/java/com/reactnativeapirtc/MainApplication.java
+++ android/app/src/main/java/com/reactnativeapirtc/MainApplication.java
@@ -3,6 +3,7 @@ package com.reactnativeapirtc;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.oney.WebRTCModule.WebRTCModulePackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new WebRTCModulePackage(),
             new RNDeviceInfo()
       );
